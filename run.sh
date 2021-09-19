68c834afb271a94288485e1e5d8b573b5f62c2af
mschrader15 <mcschrader@crimson.ua.edu>
2021-09-19 15:51:28 -0500

    update to re-run first calibration try

--- run.sh
+++ run.sh
@@ -5,10 +5,13 @@ CPU_LINES="$(grep -oP '(?>cpu_cores:).(?:\d)+' $SETTINGS_FILE)"
 CPUS="$(grep -oP '(\d)+' <<< $CPU_LINES)"
 
 source venv/bin/activate
+
+# change these for your own system
 export SIMULATION_ROOT=/home/max/remote/airport-harper-sumo
 export SENSITIVITY_ANALYSIS_OUTPUT=/home/max/tmp/airport_harper_sumo_sasumo
 export NOHUP=$SENSITIVITY_ANALYSIS_OUTPUT/nohup.out
 export NOHUP_ERROR=$SENSITIVITY_ANALYSIS_OUTPUT/nohup.err
+
 # try to shutdown existing ray instances
 ray stop
 
@@ -16,6 +19,6 @@ ray stop
 ray start --head --port=6379 --num-cpus=$CPUS
 
 # Run Simulation
-nohup python SASUMO/SASUMO/creator.py $SETTINGS_FILE > $NOHUP 2> $NOHUP_ERROR < /dev/null &
+(nohup python SASUMO/SASUMO/creator.py $SETTINGS_FILE > $NOHUP 2> $NOHUP_ERROR < /dev/null &) && (nohup ray stop)
 
 tail $NOHUP -f
