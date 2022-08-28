a7be3e3c0ef9dfb5ea84c4848b5ff039e9281a7e
mschrader15 <mcschrader@crimson.ua.edu>
2022-08-28 10:13:04 -0500

    Added helper bash scripts
    Signed-off-by: mschrader15 <mcschrader@crimson.ua.edu>

--- scripts/process_dir_results.sh
+++ scripts/process_dir_results.sh
@@ -4,5 +4,5 @@ DIRECTORIES="$1/*"
 for f in $DIRECTORIES
 do
   echo "$f"
-  python SASUMO/SASUMO/utils/process_output.py $f
+  python SASUMO/SASUMO/utils/process_output.py $f --parameter-sweep
 done
